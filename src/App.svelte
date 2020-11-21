2317270bd22bbdeb76befb7142da12a2ae5c463f
Ryan Davis <ryanscdavis@gmail.com>
2020-11-20 19:06:35 -0500

    Create drawer component

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+
+    import Drawer from './Drawer.svelte'
+    let open
+
+</script>
+
+<button on:click={() => open = true}>Open</button>
+
+<Drawer bind:open size='600px'>
+    <button on:click={() => open = false}>Close</button>
+</Drawer>
